1472473bc66e0f0a223a0405ac0bf57a38df6f72
Dhiresh Kumar <kushwahadhiresh066@gmail.com>
2024-09-25 11:54:21 +0545

    Create chaning the url form classname.js

---
+++ chaning the url form classname.js
@@ -0,0 +1,9 @@
+<script>
+// Select the anchor tag by its class name
+const links = document.getElementsByClassName('button wc-backward');
+
+// Check if any elements were found and change the href attribute of the first one
+if (links.length > 0) {
+    links[0].href = 'https://mistercake.com.np/order-online/';
+}
+</script>
