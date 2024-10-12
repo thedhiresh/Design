e130bf716348aa1a19dc5791473d964e4136962c
Dhiresh Kumar <kushwahadhiresh066@gmail.com>
2024-10-12 18:29:03 +0545

    Delete change all specific word in the page.js

--- change all specific word in the page.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-    document.addEventListener("DOMContentLoaded", function() {
-        // Replace 'oldWord' with 'newWord'
-        document.body.innerHTML = document.body.innerHTML.replace(/Select options/g, 'Buy Now');
-    });
-</script>
