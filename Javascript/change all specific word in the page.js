d9144a25a264a9291ea62df877838063d39cffd5
Dhiresh Kumar <kushwahadhiresh066@gmail.com>
2024-10-12 18:28:34 +0545

    Add files via upload

---
+++ Javascript/change all specific word in the page.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+        // Replace 'oldWord' with 'newWord'
+        document.body.innerHTML = document.body.innerHTML.replace(/Select options/g, 'Buy Now');
+    });
+</script>
